091bb19e2344ee2d9867c040f1eaabb9fbe6a741
Gabriella Köhler <gabriella.kohler91@gmail.com>
2024-03-10 12:07:00 +0000

    created a 2nd component with props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreetings';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingsWithProps';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
+      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Gabriella" age="32"/>
     </div>
   );
 }
