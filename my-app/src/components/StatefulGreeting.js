8fb5429c0525a8ee8f97997cc2c93e9be811890e
Gabriella Köhler <gabriella.kohler91@gmail.com>
2024-03-11 18:30:11 +0000

    finished the class components lesson

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -6,14 +6,38 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "Hello!",
             buttonText: "Exit",
+            count: 0
         };
     }
 
     handleClick() {
         this.setState({
-            introduction: "Goodbye!",
+            introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
+        }, ()=> {
+            console.log('new state', this.state.introduction);
+            console.log('new state', this.state.buttonText);
         });
         console.log(this.state.introduction);
+        console.log(this.state.buttonText);
+    }
+
+    increment(){
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+            return {
+                count: prevState.count +1
+            }
+        });
+    }
+
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
 
     render () {
@@ -21,6 +45,8 @@ class StatefulGreeting extends React.Component {
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
         )
     }
